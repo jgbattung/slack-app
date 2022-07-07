454bd2d9114141d29a6f09f83a7ea6d2cc11b57e
Jireh Battung <jirehgbattung@gmail.com>
2022-07-07 18:19:34 +0800

    added register functionality with userinfo being saved in localStorage. Login component not yet rendering when clicking the 'login here' link

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,17 +1,62 @@
-import logo from '/Users/jirehgbattung/AvionBatch19/slack-app/src/assets/logo.png';
+import React, { useState, useRef } from 'react';
+import Login from './Login';
+import logo from '../assets/logo.png';
 
 function Registration () {
+	const [ email, setEmail ] = React.useState<string | null>();
+	const [ password, setPassword ] = React.useState<string | null>();
+	const [ confirmPassword, setConfirmPassword ] = React.useState<string | null>();
+
+	const [ alertIncomepleteInput, setalertIncomepleteInput ] = React.useState(false);
+	const [ alertPasswordMatch, setalertPasswordMatch ] = React.useState(false);
+	const [ login, setlogin ] = React.useState(true);
+
+	// function for registering new account
+	const registerHandler = (e: any) => {
+		e.preventDefault();
+
+		const pass = e.target.password.value;
+		const confirmPass = e.target.confirmPassword.value;
+		console.log(`pw: ${pass}`);
+		console.log(`confirm pw: ${confirmPass}`);
+
+		if (!email || !password || !confirmPassword) {
+			setalertIncomepleteInput(true);
+			if (alertPasswordMatch) {
+				setalertPasswordMatch(false);
+			}
+		} else if (confirmPass !== pass) {
+			setalertPasswordMatch(true);
+			if (alertIncomepleteInput) {
+				setalertIncomepleteInput(false);
+			}
+		} else {
+			setalertIncomepleteInput(false);
+			setalertPasswordMatch(false);
+			localStorage.setItem('email', JSON.stringify(email));
+			localStorage.setItem('password', JSON.stringify(password));
+			console.log('saved to localStorage');
+			console.log('logged in');
+		}
+	};
+
+	// function for logging in if user already has an account
+	const logInHandler = () => {
+		console.log('connect to Login component');
+		<Login />; // may error na lumalabas dito
+	};
+
 	return (
 		<div>
-			<div className="w-maximum">
-				<img src={logo} alt="Slack Logo" />
+			<div className="flex justify-center w-96 m-auto">
+				<img src={logo} className="object-cover" alt="Slack Logo" />
 			</div>
 			<div className="mb-8">
 				<h2 className="flex justify-center text-5xl font-extrabold pb-3">Create a New Account</h2>
-				<p className="flex justify-center text-xl font-medium">Fill up the details below to create a new account</p>
+				<p className="flex justify-center text-xl">Register to start collaborating with your team!</p>
 			</div>
 			<div className="w-maximum">
-				<form className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4">
+				<form onSubmit={registerHandler} className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4">
 					<div className="mb-4">
 						<label className="block mt-3 text-gray-700 text-2xl font-bold mb-4" htmlFor="email">
 							Email
@@ -21,6 +66,7 @@ function Registration () {
 							id="email"
 							type="text"
 							placeholder="Enter your email"
+							onChange={(e) => setEmail(e.target.value)}
 						/>
 					</div>
 					<div className="mb-4">
@@ -32,23 +78,25 @@ function Registration () {
 							id="password"
 							type="password"
 							placeholder="Create a password"
+							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
 					<div className="mb-4">
-						<label className="block text-gray-700 text-2xl font-bold mb-4" htmlFor="cpassword">
+						<label className="block text-gray-700 text-2xl font-bold mb-4" htmlFor="confirmPassword">
 							Confirm your password
 						</label>
 						<input
 							className="shadow appearance-none text-lg border rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							id="cpassword"
+							id="confirmPassword"
 							type="password"
 							placeholder="Retype your password"
+							onChange={(e) => setConfirmPassword(e.target.value)}
 						/>
 					</div>
 					<div className="mb-6 flex justify-center text-2xl">
 						<button
-							className="bg-violet-700 w-full mt-5 transition-all hover:bg-violet-800 text-white font-medium py-3 px-6 rounded focus:outline-none focus:shadow-outline"
-							type="button"
+							className="bg-violet-700 w-full mt-5 transition-all hover:bg-violet-900 text-white font-medium py-3 px-6 rounded focus:outline-none focus:shadow-outline"
+							type="submit"
 						>
 							Create Account
 						</button>
@@ -56,11 +104,55 @@ function Registration () {
 					<div className="flex justify-center text-lg font-medium">
 						<p>
 							Already have an account?{' '}
-							<span className="text-violet-700 font-bold">
-								<a href={'https://stackoverflow.com/questions/18913559/reactjs-anchor-tag-link'}>Login here.</a>
+							<span className="text-violet-500 font-bold transition-all hover:text-violet-800">
+								<a href="#" onClick={logInHandler}>
+									Login here.
+								</a>
 							</span>
 						</p>
 					</div>
+
+					{alertIncomepleteInput && (
+						<div
+							className="bg-red-100 border text-lg mt-5 border-red-400 text-red-700 px-4 py-3 rounded relative"
+							role="alert"
+						>
+							<strong className="font-bold">Error! </strong>
+							<span className="block sm:inline"> Please accomplish all fields</span>
+							<span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+								<svg
+									className="fill-current h-6 w-6 text-red-500"
+									role="button"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 20 20"
+								>
+									<title>Close</title>
+									<path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+								</svg>
+							</span>
+						</div>
+					)}
+
+					{alertPasswordMatch && (
+						<div
+							className="bg-red-100 border text-lg mt-5 border-red-400 text-red-700 px-4 py-3 rounded relative"
+							role="alert"
+						>
+							<strong className="font-bold">Error! </strong>
+							<span className="block sm:inline"> Passwords do not match</span>
+							<span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+								<svg
+									className="fill-current h-6 w-6 text-red-500"
+									role="button"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 20 20"
+								>
+									<title>Close</title>
+									<path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+								</svg>
+							</span>
+						</div>
+					)}
 				</form>
 			</div>
 		</div>
