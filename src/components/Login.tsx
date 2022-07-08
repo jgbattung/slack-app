60e2fa72c91e3c90ebc59ebad2b595adc23e7266
Jireh Battung <jirehgbattung@gmail.com>
2022-07-08 18:07:01 +0800

    edited styles for div fullscreen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -48,55 +48,57 @@ const Login = (props: any) => {
 
 	return (
 		<div>
-			<div className="flex justify-center w-96 m-auto">
-				<img src={logo} className="object-cover" alt="Slack logo" />
-			</div>
+			<div className="mt-40">
+				<div className="flex justify-center w-96 m-auto">
+					<img src={logo} className="object-cover" alt="Slack logo" />
+				</div>
 
-			<div className="mb-8">
-				<h2 className="flex justify-center text-5xl font-extrabold pb-3">Login to Slack</h2>
-				<p className="flex justify-center text-xl">Start collaborating with your team!</p>
-			</div>
+				<div className="mb-8">
+					<h2 className="flex justify-center text-5xl font-extrabold pb-3">Login to Slack</h2>
+					<p className="flex justify-center text-xl">Start collaborating with your team!</p>
+				</div>
 
-			<div className="w-maximum">
-				<form className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-					<div className="mb-4">
-						{logInResponse.data && <h2 className="text-l bg-green-400 flex justify-center items-center">Success</h2>}
-						<label className="block mt-3 text-gray-700 text-2xl font-bold mb-4" htmlFor="email">
-							Email
-						</label>
-						<input
-							className="shadow mb-5 appearance-none text-lg border rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							id="email"
-							type="email"
-							placeholder="Enter your email"
-							name="email"
-							value={formData.email}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className="mb-2">
-						<label className="block text-gray-700 text-2xl font-bold mb-4" htmlFor="password">
-							Password
-						</label>
-						<input
-							className="shadow mb-5 appearance-none text-lg border rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							id="password"
-							type="password"
-							placeholder="Enter your password"
-							name="password"
-							value={formData.password}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className="mb-4 flex justify-center text-2xl">
-						<button
-							className="bg-violet-700 w-full mt-5 transition-all hover:bg-violet-900 text-white font-medium py-3 px-6 rounded focus:outline-none focus:shadow-outline"
-							type="submit"
-						>
-							Log In
-						</button>
-					</div>
-				</form>
+				<div className="w-maximum m-auto">
+					<form className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+						<div className="mb-4">
+							{logInResponse.data && <h2 className="text-l bg-green-400 flex justify-center items-center">Success</h2>}
+							<label className="block mt-3 text-gray-700 text-2xl font-bold mb-4" htmlFor="email">
+								Email
+							</label>
+							<input
+								className="shadow mb-5 appearance-none text-lg border rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="email"
+								type="email"
+								placeholder="Enter your email"
+								name="email"
+								value={formData.email}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="mb-2">
+							<label className="block text-gray-700 text-2xl font-bold mb-4" htmlFor="password">
+								Password
+							</label>
+							<input
+								className="shadow mb-5 appearance-none text-lg border rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="password"
+								type="password"
+								placeholder="Enter your password"
+								name="password"
+								value={formData.password}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="mb-4 flex justify-center text-2xl">
+							<button
+								className="bg-violet-700 w-full mt-5 transition-all hover:bg-violet-900 text-white font-medium py-3 px-6 rounded focus:outline-none focus:shadow-outline"
+								type="submit"
+							>
+								Log In
+							</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
