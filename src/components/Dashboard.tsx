da7b968465cb73e75554deabf1bb2bf7b6f1582f
Jireh Battung <jirehgbattung@gmail.com>
2022-07-13 17:50:07 +0800

    copied joco code for listing users and added more styling to dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,31 +1,82 @@
+import { useEffect, useState } from 'react';
+import DirectMessage from './DirectMessage';
+import RealTimeChat from './RealTimeChat';
+import logo from '../assets/logo-white.png';
+
 function Dashboard () {
+	const [ whoToChat, setWhoToChat ] = useState({
+		uid: ''
+	});
+
+	useEffect(
+		() => {
+			// call the send message utility function here
+			console.log(whoToChat.uid);
+		},
+		[ whoToChat ]
+	);
+
 	return (
-		<div className="w-screen h-screen grid grid-rows-7 grid-cols-8">
-			{/* <div className="col-span-3 row-span-1">Header</div>
-			<div className="row-span-2 border-2 border-solid border-white bg-purple-900 text-white text-xl font-medium">
-				<div className="flex justify-left mt-4 ml-6">Channels</div>
-			</div>
-			<div className="row-span-6 col-span-2 border-2 border-solid border-white bg-white text-white">Chat Field</div>
-			<div className="row-span-5 border-2 border-solid border-white bg-purple-900 text-white text-xl font-medium">
-				Direct Message
-			</div> */}
-			<div className="flex justify-center items-center text-white row-span-1 col-span-8 bg-fuchsia-900 border border-t-0 border-b-0 border-l-0 border-solid border-white">
-				HEADER
+		<div className="w-screen h-screen grid grid-rows-7 grid-cols-8 bg-white">
+			<div className="grid grid-cols-3 place-content-around text-white row-span-1 col-span-8 bg-fuchsia-900 border border-t-0 border-b-0 border-l-0 border-solid border-white">
+				<div className="ml-10 place-self-start">
+					<img src={logo} className="w-32" alt="Slack Logo White" />
+				</div>
+				<div className="place-self-center text-black text-sm">
+					<input
+						type="text"
+						placeholder="Search"
+						className="p-2 w-80 bg-fuchsia-200 placeholder-gray-500 rounded-lg focus:outline-none focus:ring focus:ring-fuchsia-600"
+					/>
+				</div>
+				<button className="mr-10 place-self-end text-sm hover:text-slate-200 cursor-pointer transition-all">Logout</button>
 			</div>
 			<div className="row-span-2 col-span-1 bg-fuchsia-800 border border-solid border-b-0 border-l-0 border-white">
-				<div className="flex justify-left mt-4 ml-6 text-white text-xl font-medium">Channels</div>
+				<div className="flex justify-left mt-4 ml-6 text-white text-md font-medium">Channels</div>
 			</div>
-			<div className="row-span-6 col-span-7 border border-solid border-white bg-white">
-				<div className="bg-white border border-solid border-b-1 border-t-0 border-l-0 border-r-0">
-					<div className="ml-6 pt-4 pb-4">Chat Name</div>
+			<div className="row-span-5 col-span-7 border border-solid border-white bg-white">
+				<div className="bg-white border border-zinc-400 border-solid border-b-1 border-t-0 border-l-0 border-r-0">
+					<div className="ml-6 pt-4 pb-4 text-lg font-bold">Channel Name or Contact Name</div>
 				</div>
 				<div className="mt-4 ml-6">Chat Field</div>
 			</div>
+
 			<div className="row-span-4 col-span-1 bg-fuchsia-800 border border-solid border-l-0 border-white">
-				<div className="flex justify-left mt-4 ml-6 text-white text-xl font-medium">Direct Messages</div>
+				<div className="flex justify-left mt-4 ml-6 text-white text-md font-medium">Direct Messages</div>
+			</div>
+			<div className="grid place-content-center col-span-7 m-2 bg-white border-2 border-gray-300 rounded-2xl">
+				<input type="text" placeholder="Message username/channel" className="rounded-md w-fit" />
 			</div>
 		</div>
 	);
 }
 
 export default Dashboard;
+
+// import { useEffect, useState } from 'react';
+// import DirectMessage from './DirectMessage';
+// import RealTimeChat from './RealTimeChat';
+
+// function Dashboard () {
+// 	const [ whoToChat, setWhoToChat ] = useState({
+// 		uid: ''
+// 	});
+// 	useEffect(
+// 		() => {
+// 			// call the send message utility function here
+// 			console.log(whoToChat.uid);
+// 		},
+// 		[ whoToChat ]
+// 	);
+
+// 	return (
+// 		<div className="grid grid-cols-3 w-full">
+// 			<div>Channel</div>
+// 			<div>RealTimeChat</div>
+// 			<div>{whoToChat.uid}</div>
+// 			<DirectMessage setWhoToChat={setWhoToChat} />
+// 		</div>
+// 	);
+// }
+
+// export default Dashboard;
--- src/components/DirectMessage.tsx
+++ src/components/DirectMessage.tsx
@@ -1,7 +1,74 @@
-function DirectMessage () {
-    return (
-        <div>DirectMessage</div>
-    )
+import { MouseEventHandler, useEffect, useState } from 'react';
+import listOfAllUsers from '../utilities/listOfAllUsers';
+
+// ------API type
+interface apiResponseTypes {
+	success?: boolean;
+	data: [];
+	errors?: any;
+}
+
+interface listOfUsersParams {
+	access_token: string;
+	client: string;
+	data?: {};
+	errors?: [];
+	expiry: string;
+	success?: boolean;
+	uid: string;
+}
+
+interface directMessageProps {
+	setWhoToChat: React.Dispatch<React.SetStateAction<{ uid: string }>>;
+}
+// ------API type
+
+function DirectMessage (props: directMessageProps) {
+	const { setWhoToChat } = props;
+
+	// ------API register user
+	const [ userData, setUserData ] = useState<apiResponseTypes>({
+		data: []
+	});
+
+	useEffect(() => {
+		console.log('fetching');
+		const userData = JSON.parse(localStorage.getItem('userLogIn') || '{}');
+		console.log(userData);
+
+		const getUsers = async (userData: listOfUsersParams) => {
+			const response = await listOfAllUsers(userData);
+			setUserData(response);
+		};
+		getUsers(userData);
+	}, []);
+
+	function handleChat (e: any) {
+		console.log('start real time chat with', e.target.name, ' as its uid');
+		// The sendMessage utility function
+		setWhoToChat({ uid: e.target.name });
+	}
+
+	const listOfUsers = userData.data.map((user: any, index: number) => {
+		return (
+			<button
+				className="rounded-full bg-purple-300 p-1 m-1 hover:ring-1 hover:ring-purple-600"
+				key={user.id}
+				onClick={handleChat}
+				name={user.uid}
+			>
+				{user.uid}
+			</button>
+		);
+	});
+	console.log(listOfUsers);
+
+	return (
+		<div className="flex flex-col justify-center items-start">
+			<div>DirectMessage</div>
+			{listOfUsers.length === 0 ? <div>Loading...</div> : listOfUsers}
+		</div>
+	);
 }
 
 export default DirectMessage;
