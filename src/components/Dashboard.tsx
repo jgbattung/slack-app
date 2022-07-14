0d916da63538ccbe43b8e2951a338100c70678cb
Jireh Battung <jirehgbattung@gmail.com>
2022-07-14 18:48:15 +0800

    added styles for dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -29,10 +29,33 @@ function Dashboard () {
 						className="p-2 w-80 bg-fuchsia-200 placeholder-gray-500 rounded-lg focus:outline-none focus:ring focus:ring-fuchsia-600"
 					/>
 				</div>
-				<button className="mr-10 place-self-end text-sm hover:text-slate-200 cursor-pointer transition-all">Logout</button>
+				<div className="flex flex-row-reverse">
+					<div className="mr-10 place-self-end text-yellow-300 hover:text-yellow-600 cursor-pointer transition-all">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							strokeWidth={2}
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+							/>
+						</svg>
+					</div>
+					<div className="mr-4 place-self-end">User ID</div>
+				</div>
 			</div>
 			<div className="row-span-2 col-span-1 bg-fuchsia-800 border border-solid border-b-0 border-l-0 border-white">
-				<div className="flex justify-left mt-4 ml-6 text-white text-md font-medium">Channels</div>
+				<div className="flex items-center">
+					<div className="flex justify-left mt-4 ml-6 text-white text-md font-medium">Channels</div>
+					<div className="flex justify-right mt-4 ml-2 text-yellow-300 text-2xl font-black hover:text-yellow-600 cursor-pointer transition-all">
+						&#43;
+					</div>
+				</div>
 			</div>
 			<div className="row-span-5 col-span-7 border border-solid border-white bg-white">
 				<div className="bg-white border border-zinc-400 border-solid border-b-1 border-t-0 border-l-0 border-r-0">
@@ -42,15 +65,28 @@ function Dashboard () {
 			</div>
 
 			<div className="row-span-4 col-span-1 bg-fuchsia-800 border border-solid border-l-0 border-white">
-				<div className="flex justify-left mt-4 ml-6 text-white text-md font-medium">Direct Messages</div>
+				<div className="flex items-center">
+					<div className="flex justify-left mt-4 ml-6 text-white text-md font-medium">Direct Message</div>
+					<div className="flex flex-row-reverse mt-4 ml-2 text-yellow-300 text-2xl font-black hover:text-yellow-600 cursor-pointer transition-all">
+						&#43;
+					</div>
+				</div>
 			</div>
-			{/* <div className="grid place-content-center col-span-7 m-2 bg-white border-2 border-gray-300 rounded-2xl"> */}
-			<textarea
-				// type="text"
-				placeholder="Message username/channel"
-				className="rounded-2xl w-full bg-white col-span-7 border-2 border-gray-200 transition-all p-3 align-top focus:outline-none focus:ring ring-1 focus:ring-gray-600"
-			/>
-			{/* </div> */}
+			<div className="w-full col-span-7 relative mb-4">
+				<textarea
+					placeholder="Message username/channel"
+					className="rounded-2xl w-full h-full bg-white col-span-7 border-2 border-gray-200 transition-all p-3 align-top focus:outline-none focus:ring ring-1 focus:ring-gray-600"
+				/>
+				<button className="bg-fuchsia-700 absolute top-0 right-0 h-full transition-all hover:bg-fuchsia-900 text-white font-semibold py-3 px-6 rounded-xl focus:outline-none focus:shadow-outline">
+					<svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+						<path
+							fillRule="evenodd"
+							d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</button>
+			</div>
 		</div>
 	);
 }
